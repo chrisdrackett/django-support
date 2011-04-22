3367b21dfb9cd0a82552ed523461d8115058e23b
Chris Drackett <chris@chrisdrackett.com>
2011-04-22 15:11:03 -0500

    pypi whynot

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+ 
+setup(
+    name='django-support',
+    version='0.1',
+    description='Support filters/tags/etc. for django.',
+    author='Chris Drackett',
+    url = "https://github.com/chrisdrackett/django-support",
+    classifiers = [
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ]
+)
