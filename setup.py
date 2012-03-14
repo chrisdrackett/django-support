7999c2396e869c6942d7b2388c3555046ad28ce4
Chris Drackett <chris@chrisdrackett.com>
2012-03-13 21:39:45 -0500

    bump to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
  
 setup(
     name='django-support',
-    version='0.1',
+    version='0.2',
     description='Support filters/tags/etc. for django.',
     classifiers = [
         "Environment :: Web Environment",
