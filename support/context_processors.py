64810982b4390fa1e51af0f3ec182d409d2809c4
Chris Drackett <chris@drackett.com>
2012-06-27 00:17:27 -0500

    context processor for compile (no function in this app yet)

--- support/context_processors.py
+++ support/context_processors.py
@@ -1,4 +1,7 @@
-from .settings import APP_NAME
+from .settings import APP_NAME, COMPILE_CSS
 
 def app_name(request):
     return {'APP_NAME': APP_NAME}
+
+def COMPILE_CSS(request):
+    return {'COMPILE_CSS': COMPILE_CSS}
