aae45b3653657f0375714993a4a3701b93c9b4d4
Chris Drackett <chris@drackett.com>
2012-06-27 00:18:49 -0500

    maybe perhaps that should be lowercase.

--- support/context_processors.py
+++ support/context_processors.py
@@ -3,5 +3,5 @@ from .settings import APP_NAME, COMPILE_CSS
 def app_name(request):
     return {'APP_NAME': APP_NAME}
 
-def COMPILE_CSS(request):
+def compile_css(request):
     return {'COMPILE_CSS': COMPILE_CSS}
