898b6a10ced413d3c2128118ab9d6e58f7a24c9d
Chris Drackett <chris@drackett.com>
2012-07-18 12:45:20 -0500

    allow passing of custom form dict.

--- support/forms/processing.py
+++ support/forms/processing.py
@@ -1,28 +1,28 @@
-def process_form(request, form_class, save_kwargs=None, *a, **kw):
+def process_form(request, form_class, form_dict=None, save_kwargs=None, *a, **kw):
     '''
         Optional:
         =========
-        
+
         You can pass kwargs to the form save function in a 'save_kwargs' dict.
-        
-        
+
+
         Example Usage:
         ==============
-        
+
         success, form = process_form(request, DiscussionForm, initial={
             'category':Category.objects.all()[0].id,
         })
         if success:
             return form # this is the return from the forms save method.
-        
+
         return template(request, 'talk/create_discussion.html', {
             'categories': Category.objects.all(),
             'form': form
         })
     '''
-    
-    form = form_class(request.POST or None, *a, **kw)
-    
+
+    form = form_class(form_dict or request.POST or None, *a, **kw)
+
     if request.method == 'POST':
         if form.is_valid():
             if not save_kwargs:
