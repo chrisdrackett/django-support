aa310b05b26d5bdcc515082448e7f8b7bd3fdbf4
Chris Drackett <chris@drackett.com>
2014-03-03 16:05:19 -0800

    ability to handle status codes.

--- support/views.py
+++ support/views.py
@@ -4,4 +4,11 @@ from django.shortcuts import render_to_response
 def template(request, template_name, dictionary, *args, **kwargs):
     kwargs['context_instance'] = RequestContext(request)
 
-    return render_to_response(template_name, dictionary, *args, **kwargs)
+    status = kwargs.pop('status', None)
+
+    response = render_to_response(template_name, dictionary, *args, **kwargs)
+
+    if status:
+        response.status_code = status
+
+    return response
